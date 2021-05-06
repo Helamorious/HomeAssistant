9c6dba0f9597bd3a730963090d07fb5f19e986d5
Michael Raison <michael.raison@helamorious.net>
2021-05-06 17:10:11 +0930

    Update travis scripts

--- travis_scripts/circadian_lighting.sh
+++ travis_scripts/circadian_lighting.sh
@@ -2,4 +2,5 @@
 set -ev
 mkdir temp
 git clone https://github.com/claytonjn/hass-circadian_lighting.git ./temp/
+ls -la ./temp/
 cp -r ./temp/hass-circadian_lighting/custom_components/* /config/custom_components/
