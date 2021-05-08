5ed791115c79c227792b97f2850d895df3cad159
Michael Raison <michael.raison@helamorious.net>
2021-05-08 15:06:37 +0930

    Adding dummy entities for travis

--- travis_scripts/circadian_lighting.sh
+++ travis_scripts/circadian_lighting.sh
@@ -2,7 +2,4 @@
 set -ev
 mkdir temp
 git clone https://github.com/claytonjn/hass-circadian_lighting.git ./temp/
-ls -la ./temp
 cp -R ./temp/custom_components /config/
-ls -la /config/custom_components
-ls -la /config/custom_components/circadian_lighting
