70aa97e63240614c46aadd10190c3829f2cae389
Michael Raison <michael.raison@helamorious.net>
2021-05-08 13:40:51 +0930

    Validating dir structure

--- travis_scripts/circadian_lighting.sh
+++ travis_scripts/circadian_lighting.sh
@@ -2,6 +2,4 @@
 set -ev
 mkdir temp
 git clone https://github.com/claytonjn/hass-circadian_lighting.git ./temp/
-cp ./temp/custom_components/* /config/custom_components/
-ls -la /config/custom_components/
-ls -la /config/custom_components/circadian_lighting/
+cp -R ./temp/custom_components/circadian_lighting /config/custom_components/
